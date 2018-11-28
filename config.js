9796f24350e7228aaf9cc44594f3d2473bf49472
Franzferdinan51 <Franzferdinan51@gmail.com>
2018-11-28 07:49:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var api = 'http://127.0.0.1:11898';
 var donationAddress = "";
-var blockTargetInterval = 30;
+var blockTargetInterval = 60;
 var coinUnits = 100;
-var symbol = 'GRUB';
+var symbol = 'MAGI';
 var refreshDelay = 30000;
 // pools stats by MainCoins
 var networkStat = {
